fa939bfd4a1102f1de697f76e033608286521cfa
Neil Kristoffer S. Orca <orcaneil9@gmail.com>
2026-09-03 11:44:35 +0800

    update projects

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["CvSU Student", "ComSci Student", "3rd Year College"],
+    strings: ["CvSU Student", "ComSci Student", "Web Developer", "Software Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
